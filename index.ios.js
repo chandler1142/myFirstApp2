f9de9cddae784689f05a14cb647bd66c40867fb9
chandler <chandler@sap.com>
2017-07-26 23:43:33 +0800

    father and child

--- index.ios.js
+++ index.ios.js
@@ -12,11 +12,18 @@ import {
     View
 } from 'react-native';
 
-export default class myFirstApp extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {times: 0}
-    }
+var Son = React.createClass({
+
+    getDefaultProps() {
+        console.log("child","getDefaultProps")
+    },
+
+    getInitialState() {
+        console.log("child","getInitialState")
+        return {
+            times: this.props.times
+        }
+    },
 
     timesPlus() {
         let times = this.state.times
@@ -24,45 +31,145 @@ export default class myFirstApp extends Component {
         this.setState({
             times: times
         })
-    }
+    },
 
     componentWillMount() {
-        console.log("componentWillMount")
-    }
+        console.log("child","componentWillMount")
+    },
 
     componentDidMount() {
-        console.log("componentDidMount")
-    }
+        console.log("child","componentDidMount")
+    },
+
+    componentWillReceiveProps(props) {
+        console.log(this.props.times + "    " + props.times)
+        console.log("child","componentWillReceiveProps")
+        this.setState({
+            times: props.times
+        })
+    },
 
     shouldComponentUpdate() {
-        console.log("shouldComponentUpdate")
+        console.log("child","shouldComponentUpdate")
         return true
-    }
+    },
 
     componentWillUpdate() {
-        console.log("componentWillUpdate")
+        console.log("child","componentWillUpdate")
 
-    }
+    },
 
     componentDidUpdate() {
-        console.log("componentDidUpdate")
+        console.log("child","componentDidUpdate")
 
+    },
+
+    timesReset() {
+        this.props.timesReset();
+    },
+
+    render() {
+        console.log("child","render")
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome} onPress={this.timesPlus}>
+                    儿子： 有本事揍我啊
+                </Text>
+                <Text style={styles.instructions}>
+                    你居然揍我: {this.state.times}次
+                </Text>
+                <Text style={styles.instructions} onPress={this.timesReset}>
+                    信不信我亲亲你
+                </Text>
+            </View>
+        )
     }
+})
+
+
+var myFirstApp = React.createClass({
+    getDefaultProps() {
+        console.log("father", "getDefaultProps")
+    },
+
+    getInitialState() {
+        console.log("father", "getInitialState")
+        return {
+            times: 2,
+            hit: false
+        }
+    },
+
+    timesReset() {
+        this.setState({
+            times: 0
+        })
+    },
+
+    willHit() {
+        this.setState({
+            hit: true
+        })
+    },
+
+    timesPlus() {
+        var times = this.state.times
+        times += 3
+        this.setState({
+            times: times
+        })
+    },
+
+    componentWillMount() {
+        console.log("father", "componentWillMount")
+    },
+
+    componentDidMount() {
+        console.log("father", "componentDidMount")
+    },
+
+    shouldComponentUpdate() {
+        console.log("father", "shouldComponentUpdate")
+        return true
+    },
+
+    componentWillUpdate() {
+        console.log("father", "componentWillUpdate")
+
+    },
+
+    componentDidUpdate() {
+        console.log("father", "componentDidUpdate")
+
+    },
 
     render() {
         console.log("render")
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome} onPress={this.timesPlus.bind(this)}>
-                    有本事点我一下
+                {
+                    this.state.hit
+                        ? <Son times={this.state.times} timesReset={this.timesReset} />
+                        : null
+                }
+
+                <Text style={styles.welcome} onPress={this.timesReset}>
+                    父亲说，心情好就放你一马
+                </Text>
+                <Text style={styles.instructions} onPress={this.willHit}>
+                    到底揍不揍
                 </Text>
                 <Text style={styles.instructions}>
-                    你点了我{this.state.times}次
+                    揍了{this.state.times}次而已
+                </Text>
+                <Text style={styles.instructions} onPress={this.timesPlus}>
+                    不听话，再揍你3次
                 </Text>
             </View>
         );
     }
-}
+})
+
 
 const styles = StyleSheet.create({
     container: {
