0201387feafdbe46bff27ddf481b602e7a15c9e2
chandler <chandler@sap.com>
2017-07-26 22:36:42 +0800

    times

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,83 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
 export default class myFirstApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {times: 0}
+    }
+
+    timesPlus() {
+        let times = this.state.times
+        times++
+        this.setState({
+            times: times
+        })
+    }
+
+    componentWillMount() {
+        console.log("componentWillMount")
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount")
+    }
+
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate")
+        return true
+    }
+
+    componentWillUpdate() {
+        console.log("componentWillUpdate")
+
+    }
+
+    componentDidUpdate() {
+        console.log("componentDidUpdate")
+
+    }
+
+    render() {
+        console.log("render")
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome} onPress={this.timesPlus.bind(this)}>
+                    有本事点我一下
+                </Text>
+                <Text style={styles.instructions}>
+                    你点了我{this.state.times}次
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('myFirstApp', () => myFirstApp);
